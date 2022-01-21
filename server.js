f5e78753239882c5609314a395f444a84acade22
Maurya-Grover <maurya.grover@gmail.com>
2022-01-22 03:55:40 +0530

    port updates

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ async function fetchDatabase() {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(6969);
+app.listen(process.env.PORT || 6969);
 
 function passwordProtected(req, res, next) {
 	res.set('WWW-Authenticate', 'Basic realm="Todo App"');
